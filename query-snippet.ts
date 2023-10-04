22546c9ffed9562937d79c0f7db6a0f0a2cbd585
asardon <as@myso.finance>
2023-10-04 12:05:45 +0200

    modified to retrieve overall loans per lender per token as well as number of loans per lender; and vice versa for borrowers

--- query-snippet.ts
+++ query-snippet.ts
@@ -1,151 +1,194 @@
 import { ethers } from 'hardhat'
 
-async function main() {
-  const borrowerGatewayAbi = [
+const borrowerGatewayAbi = [
+{
+    "anonymous": false,
+    "inputs": [
     {
-      "anonymous": false,
-      "inputs": [
+        "indexed": true,
+        "internalType": "address",
+        "name": "vaultAddr",
+        "type": "address"
+    },
+    {
+        "indexed": true,
+        "internalType": "address",
+        "name": "borrower",
+        "type": "address"
+    },
+    {
+        "components": [
         {
-          "indexed": true,
-          "internalType": "address",
-          "name": "vaultAddr",
-          "type": "address"
+            "internalType": "address",
+            "name": "borrower",
+            "type": "address"
         },
         {
-          "indexed": true,
-          "internalType": "address",
-          "name": "borrower",
-          "type": "address"
+            "internalType": "address",
+            "name": "collToken",
+            "type": "address"
         },
         {
-          "components": [
-            {
-              "internalType": "address",
-              "name": "borrower",
-              "type": "address"
-            },
-            {
-              "internalType": "address",
-              "name": "collToken",
-              "type": "address"
-            },
-            {
-              "internalType": "address",
-              "name": "loanToken",
-              "type": "address"
-            },
-            {
-              "internalType": "uint40",
-              "name": "expiry",
-              "type": "uint40"
-            },
-            {
-              "internalType": "uint40",
-              "name": "earliestRepay",
-              "type": "uint40"
-            },
-            {
-              "internalType": "uint128",
-              "name": "initCollAmount",
-              "type": "uint128"
-            },
-            {
-              "internalType": "uint128",
-              "name": "initLoanAmount",
-              "type": "uint128"
-            },
-            {
-              "internalType": "uint128",
-              "name": "initRepayAmount",
-              "type": "uint128"
-            },
-            {
-              "internalType": "uint128",
-              "name": "amountRepaidSoFar",
-              "type": "uint128"
-            },
-            {
-              "internalType": "uint128",
-              "name": "amountReclaimedSoFar",
-              "type": "uint128"
-            },
-            {
-              "internalType": "bool",
-              "name": "collUnlocked",
-              "type": "bool"
-            },
-            {
-              "internalType": "address",
-              "name": "collTokenCompartmentAddr",
-              "type": "address"
-            }
-          ],
-          "indexed": false,
-          "internalType": "struct DataTypesPeerToPeer.Loan",
-          "name": "loan",
-          "type": "tuple"
+            "internalType": "address",
+            "name": "loanToken",
+            "type": "address"
         },
         {
-          "indexed": false,
-          "internalType": "uint256",
-          "name": "upfrontFee",
-          "type": "uint256"
+            "internalType": "uint40",
+            "name": "expiry",
+            "type": "uint40"
         },
         {
-          "indexed": true,
-          "internalType": "uint256",
-          "name": "loanId",
-          "type": "uint256"
+            "internalType": "uint40",
+            "name": "earliestRepay",
+            "type": "uint40"
         },
         {
-          "indexed": false,
-          "internalType": "address",
-          "name": "callbackAddr",
-          "type": "address"
+            "internalType": "uint128",
+            "name": "initCollAmount",
+            "type": "uint128"
         },
         {
-          "indexed": false,
-          "internalType": "bytes",
-          "name": "callbackData",
-          "type": "bytes"
+            "internalType": "uint128",
+            "name": "initLoanAmount",
+            "type": "uint128"
+        },
+        {
+            "internalType": "uint128",
+            "name": "initRepayAmount",
+            "type": "uint128"
+        },
+        {
+            "internalType": "uint128",
+            "name": "amountRepaidSoFar",
+            "type": "uint128"
+        },
+        {
+            "internalType": "uint128",
+            "name": "amountReclaimedSoFar",
+            "type": "uint128"
+        },
+        {
+            "internalType": "bool",
+            "name": "collUnlocked",
+            "type": "bool"
+        },
+        {
+            "internalType": "address",
+            "name": "collTokenCompartmentAddr",
+            "type": "address"
         }
-      ],
-      "name": "Borrowed",
-      "type": "event"
+        ],
+        "indexed": false,
+        "internalType": "struct DataTypesPeerToPeer.Loan",
+        "name": "loan",
+        "type": "tuple"
+    },
+    {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "upfrontFee",
+        "type": "uint256"
+    },
+    {
+        "indexed": true,
+        "internalType": "uint256",
+        "name": "loanId",
+        "type": "uint256"
+    },
+    {
+        "indexed": false,
+        "internalType": "address",
+        "name": "callbackAddr",
+        "type": "address"
+    },
+    {
+        "indexed": false,
+        "internalType": "bytes",
+        "name": "callbackData",
+        "type": "bytes"
     }
-  ]
+    ],
+    "name": "Borrowed",
+    "type": "event"
+}
+]
 
-  const borrowerGateway = await ethers.getContractAt(borrowerGatewayAbi, '0xd8b132A0abA610D0AaA18716A2b26e14141f112C')
+const lenderVaultAbi = [
+    {
+       "type":"function",
+       "stateMutability":"view",
+       "outputs":[
+          {
+             "type":"address",
+             "name":"",
+             "internalType":"address"
+          }
+       ],
+       "name":"owner",
+       "inputs":[
+          
+       ]
+    }
+ ]
 
-  aggregateLoanStats(borrowerGateway)
+async function main() {
+    const borrowerGateway = await ethers.getContractAt(borrowerGatewayAbi, '0xd8b132A0abA610D0AaA18716A2b26e14141f112C')
+    aggregateLoanStats(borrowerGateway)
 }
 
 async function aggregateLoanStats(borrowerGateway: any) {
-  const filter = borrowerGateway.filters.Borrowed()
+    const filter = borrowerGateway.filters.Borrowed()
 
-  const fromBlock = 3470992
-  const toBlock = 'latest'
-  
-  const events = await borrowerGateway.queryFilter(filter, fromBlock, toBlock)
-  const aggregatedLoans: any = {}
+    const fromBlock = 3470992
+    const toBlock = 'latest'
 
-  for (let event of events) {
-      const lender = event.args.vaultAddr
-      const loanCurrency = event.args.loan.loanToken
-      const initLoanAmount = event.args.loan.initLoanAmount
+    const events = await borrowerGateway.queryFilter(filter, fromBlock, toBlock)
+    const aggregatedLoansByBorrower: any = {}
+    const aggregatedLoansByLender: any = {}
+    const borrowerCounts: any = {}
+    const lenderCounts: any = {}
 
-      if (!aggregatedLoans[lender]) {
-          aggregatedLoans[lender] = {}
-      }
+    for (let event of events) {
+        const borrower = event.args.borrower
+        const lenderVault = event.args.vaultAddr
+        const lenderVaultContract = await ethers.getContractAt(lenderVaultAbi, lenderVault)
+        const lender = await lenderVaultContract.owner()
+        const loanCurrency = event.args.loan.loanToken
+        const initLoanAmount = event.args.loan.initLoanAmount
 
-      if (aggregatedLoans[lender][loanCurrency]) {
-          aggregatedLoans[lender][loanCurrency] = aggregatedLoans[lender][loanCurrency].add(initLoanAmount)
-      } else {
-          aggregatedLoans[lender][loanCurrency] = initLoanAmount
-      }
-  }
+        // Aggregating loans by Borrower
+        if (!aggregatedLoansByBorrower[borrower]) {
+            aggregatedLoansByBorrower[borrower] = {}
+        }
+        if (aggregatedLoansByBorrower[borrower][loanCurrency]) {
+            aggregatedLoansByBorrower[borrower][loanCurrency] = aggregatedLoansByBorrower[borrower][loanCurrency].add(initLoanAmount)
+        } else {
+            aggregatedLoansByBorrower[borrower][loanCurrency] = initLoanAmount
+        }
+        borrowerCounts[borrower] = (borrowerCounts[borrower] || 0) + 1
 
-  console.log(aggregatedLoans)
+        // Aggregating loans by Lender
+        if (!aggregatedLoansByLender[lender]) {
+            aggregatedLoansByLender[lender] = {}
+        }
+        if (aggregatedLoansByLender[lender][loanCurrency]) {
+            aggregatedLoansByLender[lender][loanCurrency] = aggregatedLoansByLender[lender][loanCurrency].add(initLoanAmount)
+        } else {
+            aggregatedLoansByLender[lender][loanCurrency] = initLoanAmount
+        }
+        lenderCounts[lender] = (lenderCounts[lender] || 0) + 1
+    }
+
+    console.log('*********** aggregated loans per borrower and token ***********')
+    console.log(aggregatedLoansByBorrower)
+    console.log('*********** number of loans per borrower ***********')
+    console.log(borrowerCounts)
+
+    console.log('*********** aggregated loans per lender and token ***********')
+    console.log(aggregatedLoansByLender)
+    console.log('*********** number of loans per lender ***********')
+    console.log(lenderCounts)
 }
 
 main().catch(error => {
